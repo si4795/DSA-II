14050bd85d9540b105d18d63932cfd98c31f4b7a
si4795 <suaibislam56@gmail.com>
2026-03-04 21:42:24 +0600

    Week01Problem01

---
+++ Week01Problem01.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the number of elements in the array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements of the array: ";
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0; i<n-1; i++)
+    {
+        int min_index = i;
+        for(int j=i+1; j<n; j++)
+        {
+            if(arr[j] < arr[min_index])
+            {
+                min_index = j;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = arr[min_index];
+        arr[min_index] = temp;
+
+    }
+    int count = 1;
+    for(int i=1; i<n; i++)
+    {
+        if(arr[i] != arr[i-1])
+        {
+            count++;
+        }
+    }
+    cout<<"The number of distinct elements in the array is: "<<count<<endl;
+    return 0;
+}
